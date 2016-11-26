d39224c6f63b6031c67e2673eee3951347ebd603
adamjelly <adamturner89@gmail.com>
2016-11-26 14:25:32 +0900

    Initial commit of a number guessing game in javascript

---
+++ guessNum.js
@@ -0,0 +1,39 @@
+// Generate a random number between 1 and 100
+var randomNumber = Math.floor(Math.random() * 100);
+
+// start of the guessing game so that while the number is unknown and the user has not exceeded 7 guesses, they can make a new guess
+$(document).ready(function(){
+  $("#guess").click(function (){
+      // ask the user for some input
+      var userGuess = prompt("Please guess a number between 1 and 100, you have 7 attempts.");
+
+      // set a variable to allow us to exit when the number has been properly guessed
+      var guess = true;
+
+      // a counter for the number of guesses made by the user
+      var numGuess = 0;
+
+      while (guess && numGuess <= 7){
+
+        if (userGuess != randomNumber){
+          if (userGuess > randomNumber) { //if the user is higher print something and then ask for a new guess
+            numGuess += 1;
+            var userGuess = prompt("Sorry " + userGuess + " was too HIGH. This was guess " + numGuess + ", please try again.");
+          }
+          else { // if the user is lower print something else
+            numGuess += 1;
+            var userGuess = prompt("Sorry " + userGuess + " was too LOW. This was guess " + numGuess + ", please try again.");
+          }
+        }
+        else { // exit when the player has successfully guessed the number
+          alert("Congratulations! You guessed the correct number, the number was " + randomNumber + ", you required " + numGuess + " guesses!");
+          guess = false;
+        }
+      }
+
+      if (numGuess == 7 && userGuess != randomNumber) {
+        alert("Sorry, you have exceeded the maximum number of guesses.  The correct answer was " + randomNumber);
+      }
+      else{}
+    });
+  });
